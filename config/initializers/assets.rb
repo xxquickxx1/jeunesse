d432548638b2fa39e4ad762e4c5682f760103d3d
Thomas <thomas@thomas-VirtualBox.(none)>
2015-04-17 18:41:17 +0100

    he

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.precompile += %w( vendor/modernizr.js jquery-tub
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+ config.assets.initialize_on_precompile = false
+
