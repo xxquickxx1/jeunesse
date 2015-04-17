662e3e6ede6e7db2d80577cfc841687f233c32c3
Thomas <thomas@thomas-VirtualBox.(none)>
2015-04-17 18:59:13 +0100

    hello

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-#Rails.application.config.assets.precompile += %w( vendor/modernizr.js jquery-tubular.js )
+Rails.application.config.assets.precompile = false
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -10,5 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
- config.assets.initialize_on_precompile = false
 
