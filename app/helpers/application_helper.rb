6e6bc1b96c5956e55bdb504e8ec6092d0d3671eb
Thomas <thomas@thomas-VirtualBox.(none)>
2015-04-22 10:23:30 +0100

    Heheh

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,16 @@ module ApplicationHelper
   presenter
 end
 
+def footer_menu
+  menu_items = Refinery::Menu.new(Refinery::Page.footer_menu_pages)
+
+  presenter = Refinery::Pages::MenuPresenter.new(menu_items, self)
+  presenter.dom_id = "footer_menu"
+  presenter.css = "footer_menu"
+  presenter.menu_tag = :div
+  presenter
+end
+
 
 
 end
