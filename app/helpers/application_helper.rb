16c4feccf0012ddd0a4f24c133df0148059dcd06
Thomas <thomas@thomas-VirtualBox.(none)>
2015-05-11 01:51:45 +0100

    hehe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def navigation_menu
   presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
-  presenter.css = "left"
+  presenter.css = "right"
   presenter.list_tag_css = ""
   presenter.selected_css = ""
   presenter.max_depth = 12 # prevents dropdown menus, which don't render correctly
